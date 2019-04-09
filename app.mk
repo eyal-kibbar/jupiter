8ca7f73e60d3c3308c447cedef1e512d53bf9e56
Eyal Kibbar <eyal.kibbar@gmail.com>
2019-04-09 09:08:31 +0300

    moved test to apps

--- app.mk
+++ app.mk
@@ -21,5 +21,5 @@ include $(ROOT_PATH)/sensors/moisture/rules.mk
 # apps
 include $(ROOT_PATH)/apps/hexpod/rules.mk
 include $(ROOT_PATH)/apps/invpend/rules.mk
-include $(ROOT_PATH)/test/rules.mk
+include $(ROOT_PATH)/apps/test/rules.mk
 include $(ROOT_PATH)/rules.mk

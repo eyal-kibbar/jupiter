eec619034711a022989edfb880db21cdc67b37e3
Eyal Kibbar <eyal.kibbar@gmail.com>
2019-04-23 13:58:20 +0300

    testing software uart implementation

--- core/ganymede/src/common/gmd_kern.c
+++ core/ganymede/src/common/gmd_kern.c
@@ -24,17 +24,12 @@ int main()
     gmd_i2c_init();
     gmd_log_init();
     gmd_sched_init();
-    
- 
+
+
     platform_sei();
-   
-    
+
+
     while (1) gmd_sched_loop();
-    
+
     return 0;
 }
-
-
-
-
-

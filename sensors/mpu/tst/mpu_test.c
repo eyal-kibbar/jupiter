a129c285089d24c7cada540c778663baf77cdd29
Eyal Kibbar <eyal.kibbar@gmail.com>
2021-04-16 10:48:25 +0300

    added mpu complementary filter + plotting script

--- sensors/mpu/tst/mpu_test.c
+++ sensors/mpu/tst/mpu_test.c
@@ -1,67 +1,107 @@
 #include "ganymede.h"
-#include "io.h"
 #include "logging.h"
+#include "io.h"
+#include "io_platform.h"
 #include "mpu.h"
-
-#include <stdio.h>
-#include <string.h>
-
-#include <avr/interrupt.h>
-//#include <avr/io.h>
-
-#include <math.h>
+#include "math.h"
+#include <avr/io.h>
 
 
 void setup()
 {
-    io_uart_init(0, 9600);
+    io_uart_init(0, 57600);
     io_logging_init();
     io_i2c_master_init();
 }
 
-static int16_t calc_angle(float x,float z)
+
+
+void init()
+{
+    mpu_init();
+    // initialize 16bit clock with 64 prescaler
+    // this gives us a 4 microseconds per tick: (16Mhz/64)^-1
+    TCCR1B = _BV(CS11) | _BV(CS10);
+    TCNT1H = 0;
+    TCNT1L = 0;
+
+    mpu_calibrate();
+}
+
+static float calc_angle(float x,float z)
 {
     float res;
 
     res = atan2(x, z) * (180 / M_PI);
 
-    return (int16_t)roundf(res);
+    return res;
 }
+
 
 mpu_data_t data;
-
-void init()
-{
-    mpu_init();
-}
+float angles[3];
+float gyro_integral_angles[3];
 
 void loop()
 {
     int16_t x, y, z;
     int16_t gx, gy, gz;
-    uint8_t val;
+    uint8_t val = 0;
+    float accel_angles[3];
+    float gyro_angles[3];
 
-    if (0 == (val = mpu_get_status())) {
-        gmd_delay(100);
+    uint16_t curr_ts;
+    uint8_t* p_curr_ts = (uint8_t*)&curr_ts;
+    float dt;
+
+    if (0 == (0x1 & (val = mpu_get_status()))) {
+        LOG_INFO(TEST, "data not ready: %02x", val);
         return;
     }
 
+    // read and clear the clock
+    p_curr_ts[0] = TCNT1L;
+    p_curr_ts[1] = TCNT1H;
+    TCNT1H = 0;
+    TCNT1L = 0;
+
     mpu_read(&data);
-    x = (int16_t)roundf(data.accel[0] * 1000);
-    y = (int16_t)roundf(data.accel[1] * 1000);
-    z = (int16_t)roundf(data.accel[2] * 1000);
 
-    gx = (int16_t)roundf(data.gyro[0] * 1000);
-    gy = (int16_t)roundf(data.gyro[1] * 1000);
-    gz = (int16_t)roundf(data.gyro[2] * 1000);
+    accel_angles[0] = calc_angle(-data.accel[0], sqrt(data.accel[1]*data.accel[1] + data.accel[2]*data.accel[2]));
+    accel_angles[1] = calc_angle(data.accel[1],  sqrt(data.accel[1]*data.accel[1] + data.accel[2]*data.accel[2]));
 
-    //mpu_get_accel(&x, &y, &z);
-    LOG_INFO(TEST, "(%6d %6d %6d) %3d %3d (%6d %6d %6d) %6d", x, y, z,
-             calc_angle(x / 1000.0f, z / 1000.0f),
-             calc_angle(y / 1000.0f, z / 1000.0f),
-            gx, gy, gz,
-            (int16_t)roundf(data.temperature));
+    // convert clock ticks to seconds (gyro output is in degrees per second)
+    dt = (curr_ts * 4.0) / 1000000.0;
 
+    gyro_angles[0] = data.gyro[1] * dt;
+    gyro_angles[1] = data.gyro[0] * dt;
+    gyro_angles[2] = data.gyro[2] * dt;
+
+    gyro_integral_angles[0] += gyro_angles[0];
+    gyro_integral_angles[1] += gyro_angles[1];
+    gyro_integral_angles[2] += gyro_angles[2];
+
+    // apply complementary filter for pitch and roll
+    angles[0] = (0.7 * (angles[0] + gyro_angles[0])) + 0.3 * accel_angles[0];
+    angles[1] = (0.7 * (angles[1] + gyro_angles[1])) + 0.3 * accel_angles[1];
+    angles[2] = angles[2] + gyro_angles[2]; // use only gyro for yaw
+
+    // print results for arduplot.py script
+    LOG_INFO(TEST, "%d,%d,%d,%d,%d,%d,%d,%d,%d",
+        (int16_t)roundf(accel_angles[0]),
+        (int16_t)roundf(gyro_integral_angles[0]),
+        (int16_t)roundf(angles[0]),
+
+        (int16_t)roundf(accel_angles[1]),
+        (int16_t)roundf(gyro_integral_angles[1]),
+        (int16_t)roundf(angles[1]),
+
+        (int16_t)roundf(gyro_integral_angles[2]),
+        (int16_t)roundf(angles[2]),
+        curr_ts);
+
+
+    gmd_delay(100);
 }
 
 

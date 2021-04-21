9ecb6ac81d4997f34d2bd4de3651d91f63ad30aa
Eyal Kibbar <eyal.kibbar@gmail.com>
2021-04-21 22:23:23 +0300

    added bmp180 driver

--- sensors/barometer/api/barometer.h
+++ sensors/barometer/api/barometer.h
@@ -1,15 +1,19 @@
 #ifndef BAROMETER_H_
 #define BAROMETER_H_
 
-typedef struct barometer_s {
-    float height_m;
-    
-} barometer_t;
+#include <stdint.h>
 
 
 void barometer_init();
 
-void barometer_get(barometer_t* out);
+uint8_t barometer_is_data_ready();
+
+
+void barometer_pressure_start();
+void barometer_temperature_start();
+
+void barometer_pressure_get(float* out);
+void barometer_temperature_get(float* out);
 
 
 #endif /* BAROMETER_H_ */
--- sensors/barometer/tst/barometer_test.c
+++ sensors/barometer/tst/barometer_test.c
@@ -1,15 +1,27 @@
 #include "ganymede.h"
 #include "logging.h"
 #include "barometer.h"
+#include "io.h"
+#include "logging.h"
 
 void setup()
+{
+	io_uart_init(0, 57600);
+    io_logging_init();
+    io_i2c_master_init();
+}
+
+void init()
 {
 	LOG_INFO(BAROMETER_TEST, "init");
 	barometer_init();
 }
-
 void loop()
 {
+	float p;
+	barometer_temperature_start();
+	barometer_temperature_get(&p);
+
 	gmd_delay(1000);
 }
 

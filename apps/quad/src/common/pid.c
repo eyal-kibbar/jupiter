ab81bb81e0c001cf9d4b20f6cdda21ce717c42cd
Eyal Kibbar <eyal.kibbar@gmail.com>
2022-03-28 22:43:17 +0300

    fixed pid bug

--- apps/quad/src/common/pid.c
+++ apps/quad/src/common/pid.c
@@ -4,18 +4,18 @@
 float pid_step(pid_config_t* cfg, pid_state_t* state, float input, float setpoint)
 {
     float output;
-    float error = input - setpoint;
-    float d_input = (error - state->prev_error);
+    float error = setpoint - input;
+    float d_input = (input - state->prev_error);
 
     state->error_integral += error;
     state->error_integral = MIN(cfg->output_limit, state->error_integral);
     state->error_integral = MAX(-cfg->output_limit, state->error_integral);
 
-    output = (cfg->kp * error) + (cfg->ki * state->error_integral) + (cfg->kd * d_input);
+    output = (cfg->kp * error) + (cfg->ki * state->error_integral) - (cfg->kd * d_input);
     output = MIN(cfg->output_limit, output);
     output = MAX(-cfg->output_limit, output);
 
-    state->prev_error = error;
+    state->prev_error = input;
 
     return output;
 }
